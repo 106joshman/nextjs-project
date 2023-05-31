8f43da37a75e4e8a4954da2de9ed52ccdfba257b
Joshua <ejembijoshman@gmail.com>
2023-05-31 10:37:48 +0100

    check netlify error cause

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 import Results from "@/components/Results/Results";
 import axios from "axios";
 
-const API_KEY = process.env.API_KEY;
+// const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
   const res = await axios(
     `https://api.themoviedb.org/3/${
       genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
-    }?api_key=${API_KEY}&language=en-US&page=1`,
+    }?api_key=${process.env.API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
 
